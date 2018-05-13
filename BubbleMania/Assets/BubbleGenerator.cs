36dda4a2c83aee2010446170a876e9d0b503cc2a
Samantha Bajis <Sammi.Bajis@gmail.com>
2018-05-12 23:17:43 -0400

    First Commit
    Chest open animation works, but want to fix it to make it better. Using Particle Effect for bubbles right now. Need to also add bubble pop ability and chest close.

---
+++ BubbleMania/Assets/BubbleGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleGenerator : MonoBehaviour {
+
+    public GameObject[] enemies;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool stop;
+
+    int randenemy;
+
+	// Use this for initialization
+	void Start () 
+    {
+        StartCoroutine(waitSpawner());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+	}
+
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (!stop)
+        {
+            randenemy = Random.Range (0, 2);
+
+            Vector3 spawnPosition = new Vector3(Random.Range (-spawnValues.x, spawnValues.x), 1, Random.Range(-spawnValues.z, spawnValues.z));
+
+            Instantiate(enemies[randenemy], spawnPosition + transform.TransformPoint (0, 0, 0), gameObject.transform.rotation);
+
+            yield return new WaitForSeconds(spawnWait);
+
+        }
+    }
+}
---
+++ BubbleMania/Assets/ChestOpener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChestOpener : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+    anim = GetComponent<Animator>();
+    }
+
+    public void OnPointerClick()
+    {
+        anim.SetTrigger("ChestOpen");
+    }
+
+}
