6cf5bb90f4b557f01bb4929ed8594327fd78a7aa
Samantha Bajis <Sammi.Bajis@gmail.com>
2018-05-13 22:57:14 -0400

    Second Commit
    Got everything working. Chest opens and closes, bubbles spawn endlessly all with users touch.

---
+++ BubbleMania/Assets/My Scripts/BubbleDestroy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleDestroy : MonoBehaviour
+{
+
+    public float LifeTime = 10f;
+
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if (LifeTime > 0)
+            {
+                LifeTime -= Time.deltaTime;
+                if (LifeTime <= 0)
+                {
+                    Destruction();
+                }
+            }
+
+            if (this.transform.position.y <= -20)
+            {
+                Destruction();
+            }
+        }
+
+        void OnCollisionEnter(Collision coll)
+        {
+            if (coll.gameObject.name == "BubbleDestroy")
+            {
+                Destruction();
+            }
+        }
+
+        void Destruction()
+        {
+            Destroy(this.gameObject);
+        }
+    }
---
+++ BubbleMania/Assets/My Scripts/BubbleSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleSpawner : MonoBehaviour {
+
+    public Transform spawnPos;
+    public GameObject [] spawnee;
+    public Vector3 spawnValues;
+
+    int randbub;
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButton(0)) 
+        {
+
+                randbub = Random.Range(0, 0);
+
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 1, Random.Range(-spawnValues.z, spawnValues.z));
+
+                Instantiate(spawnee[randbub], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+            }
+        }
+	}
